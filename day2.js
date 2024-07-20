424eb495522cb6e2421075ef70e61491e1d19e14
Priya <priyay1104@gmail.com>
2024-07-20 16:41:05 +0530

    "added ady2.js"

---
+++ day2.js
@@ -0,0 +1,103 @@
+//activity 1
+
+//task 1 
+let a = 5;
+let b = 6;
+console.log(a+b); //11
+
+//task2
+let n1 = 12;
+let n2 = 5;
+console.log(n1-n2); //7
+
+//task3
+let a1 = 4;
+let a2 = 5;
+console.log(a1*a2); //20
+
+//task4
+let b1 = 24;
+let b2 = 6;
+console.log(b1/b2); //4
+
+//task5
+let c1 = 43;
+let c2 = 7;
+console.log(c1%c2); //1
+
+//activity 2
+
+//task6
+let x1 = 5;
+let x2 = 6;
+x1 += x2 //x1 = x1 + x2
+console.log(x1) //11
+
+//task 7
+let y1 = 9;
+let  y2 = 2;
+y1 -= y2; //y1 = y1-y2
+console.log(y1) //7
+
+//activity 3
+//task 8
+let d1 = 8;
+let d2 = 10;
+console.log(d1<d2) //true
+console.log(d1>d2) //false
+
+//task 9
+let e1 = 5;
+let e2 = 6;
+let e3 = 6;
+console.log(e1>=e2); //false
+console.log(e2<=e3); //true
+
+//task 10
+let f1 = "ocd";
+let f2 = "ocd";
+console.log(f1 == f2); //true
+console.log(f1 === f2); //true
+
+//activity 4 
+
+//task11
+const num1 = 10, num2 = 3;
+console.log((num1 < 6) && (num2< 5)); //false
+
+//task12
+const num3 = 10, num4 = 3;
+console.log((num3 < 6) && (num4< 5)); // false
+
+//task13
+let num5 = 5  ;
+console.log(!(num5==5));    // false
+
+//acticity 5
+//task 14
+let number = 25;
+number>0?console.log("positive"):console.log("negative"); //positive
+
+
+//feature 1
+let g1 = 40;
+let g2 = 3;
+console.log(g1+g2); //43
+console.log(g1-g2); //37
+console.log(g1*g2); //120
+console.log(g1/g2); //13.33
+console.log(g1%g2); //1
+
+//feature 2
+let number3=20 , number4="10" ;
+let result2 = (number3 > number4) && (number3 < number4) ;
+let result3 = (number3 > number4) || (number3 > number4) ;
+let result4 = !(number3=== number4) ;
+
+console.log(result2 ) ;  // false
+console.log(result3 ) ;  // true
+console.log(result4 ) ;  // true
+
+//feature 3
+let numb = -25;
+numb>0?console.log("positive"):console.log("negative"); //negative
